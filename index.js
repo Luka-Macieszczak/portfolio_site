ed49cfc5ca5f2dfdececf1d21878f3f13f445eab
Luka <luka.macieszczak@gmail.com>
2022-12-11 18:11:50 -0500

    fade effect and links

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ Array.from(document.getElementsByClassName("menu-item"))
     });
 console.log(Array.from(document.getElementsByClassName("menu-item")))
 
-const handleMouseMove = (e) => {
-    
-}
+const cards = document.getElementById("cards")
 
-
-
-document.getElementById("cards").onmousemove = (e) => {
+cards.onmousemove = (e) => {
     for(const card of document.querySelectorAll(".card")) {
         const rect = card.getBoundingClientRect(), x = e.clientX - rect.left, y = e.clientY - rect.top;
 
@@ -23,3 +19,46 @@ document.getElementById("cards").onmousemove = (e) => {
         card.style.setProperty("--mouse-y", `${y}px`)
     }
 }
+
+for(const card of document.querySelectorAll(".card")) {
+        card.style.setProperty('--opacity-tech', '0')
+}
+
+let links = Array.from(document.getElementsByClassName('link'))
+
+links.forEach((link, index) => {
+    let x = `${((index%3) - 1) * 25}vw`
+    link.style.setProperty('--opacity', '1')
+    link.style.setProperty('--pos-x', x)
+})
+
+let isDescActive = true;
+Array.from(document.getElementsByClassName('card'))
+    .forEach((card, index) => {
+        card.onclick = () => {
+            if(isDescActive){
+                card.style.setProperty('--opacity-desc', '0')
+                card.style.setProperty('--opacity-tech', '1')
+                for(const link of links){
+                    link.style.setProperty('--opacity', '0')
+                }
+                isDescActive = false
+            } else {
+                card.style.setProperty('--opacity-desc', '1')
+                card.style.setProperty('--opacity-tech', '0')
+                for(const link of links){
+                    link.style.setProperty('--opacity', '1')
+                }
+                isDescActive = true
+            }
+        }
+    })
+
+
+
+Array.from(document.getElementsByClassName('link'))
+    .forEach((link, index) => {
+        link.onclick = () => {
+            window.open('https://directx.live')
+        }
+     })
